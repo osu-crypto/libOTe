06fbb0f7d1c9532a3a9c745d108ce27506456dbd
peter Rindal <peter Rindal>
2017-03-26 20:45:54 -0700

    script update and local build thirdparty update

--- buildAll.ps1
+++ buildAll.ps1
@@ -1,7 +1,8 @@
 $ErrorActionPreference = "Stop"
 
 # Update this if needed
-$MSBuild = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe'
+$MSBuild = 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
+#$MSBuild = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe'
 
 if(!(Test-Path $MSBuild))
 {
