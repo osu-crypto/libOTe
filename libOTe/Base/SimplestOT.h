67225a7f90ea07b7e3eba818fcd69cf6b324f663
Peter Rindal <rindalp@oregonstate.edu>
2018-09-25 19:41:31 -0700

    disabled simpleOT by default.

--- libOTe/Base/SimplestOT.h
+++ libOTe/Base/SimplestOT.h
@@ -1,9 +1,9 @@
 #pragma once
 // This file and the associated implementation has been placed in the public domain, waiving all copyright. No restrictions are placed on its use. 
 
-#if defined(__linux__) && not defined(NO_SIMPLEST_OT)
-#define ENABLE_SIMPLEST_OT
-#endif 
+//#if defined(__linux__) && not defined(NO_SIMPLEST_OT)
+//#define ENABLE_SIMPLEST_OT
+//#endif 
 
 
 #ifdef ENABLE_SIMPLEST_OT
