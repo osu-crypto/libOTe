34281e3f88091052e2617a28b91534c7f784a910
Peter Rindal <rindalp@oregonstate.edu>
2018-12-03 20:46:35 -0800

    update to circuit lib

--- libOTe/config.h
+++ libOTe/config.h
@@ -1,6 +1,9 @@
 #pragma once
 
-// build the library with simplest OT enabled
-// #define ENABLE_SIMPLESTOT ON
 
+// build the library with simplest OT enabled
+
+#ifndef _MSC_VER
+#define ENABLE_SIMPLESTOT ON
+#endif
 
