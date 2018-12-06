d61d2b802be2d0a3fda8d2e11d88ec291d46aa49
Peter Rindal <rindalp@oregonstate.edu>
2018-12-05 18:09:51 -0800

    #if def fix

--- libOTe/Base/BaseOT.h
+++ libOTe/Base/BaseOT.h
@@ -9,7 +9,7 @@ namespace osuCrypto
 
 #define LIBOTE_HAS_BASE_OT
     using DefaultBaseOT = SimplestOT;
-#elif (defined ENABLE_RELIC  || ENABLE_MIRACL)
+#elif NAOR_PINKAS
 
 #define LIBOTE_HAS_BASE_OT
     using DefaultBaseOT = NaorPinkas;
