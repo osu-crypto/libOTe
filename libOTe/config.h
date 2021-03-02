393ae5d668107aebd655bdb4d1f97da46fa301ac
Peter Rindal <peterrindal@gmail.com>
2021-03-02 09:40:26 -0800

    version 1.3

--- libOTe/config.h
+++ libOTe/config.h
@@ -4,27 +4,27 @@
 #define LIBOTE_VERSION (LIBOTE_VERSION_MAJOR * 10000 + LIBOTE_VERSION_MINOR * 100 + LIBOTE_VERSION_PATCH)
 
 // build the library with "simplest" Base OT enabled
-#define ENABLE_SIMPLESTOT ON
+/* #undef ENABLE_SIMPLESTOT */
 
 // build the library with the ASM "simplest" Base OT enabled
-#define ENABLE_SIMPLESTOT_ASM ON
+/* #undef ENABLE_SIMPLESTOT_ASM */
 
 // build the library with Masney Rindal Base OT enabled
-#define ENABLE_MR ON
+#define ENABLE_MR True
 
 // build the library with Masney Rindal Kyber Base OT enabled
-#define ENABLE_MR_KYBER ON
+/* #undef ENABLE_MR_KYBER */
 
 // build the library with Naor Pinkas Base OT enabled
-#define ENABLE_NP ON
+/* #undef ENABLE_NP */
 
 
 
 // build the library with Keller Orse Scholl OT-Ext enabled
-#define ENABLE_KOS ON
+/* #undef ENABLE_KOS */
 
 // build the library with IKNP OT-Ext enabled
-#define ENABLE_IKNP ON
+#define ENABLE_IKNP True
 
 // build the library with Silent OT Extension enabled
 #define ENABLE_SILENTOT ON
@@ -32,24 +32,24 @@
 
 
 // build the library with KOS Delta-OT-ext enabled
-#define ENABLE_DELTA_KOS ON
+/* #undef ENABLE_DELTA_KOS */
 
 // build the library with IKNP Delta-OT-ext enabled
-#define ENABLE_DELTA_IKNP ON
+/* #undef ENABLE_DELTA_IKNP */
 
 
 
 // build the library with OOS 1-oo-N OT-Ext enabled
-#define ENABLE_OOS ON
+/* #undef ENABLE_OOS */
 
 // build the library with KKRT 1-oo-N OT-Ext enabled
-#define ENABLE_KKRT ON
+/* #undef ENABLE_KKRT */
 
 // build the library with RR 1-oo-N OT-Ext OT-ext enabled
-#define ENABLE_RR ON
+/* #undef ENABLE_RR */
 
 // build the library with RR approx k-oo-N OT-ext enabled
-#define ENABLE_AKN ON
+/* #undef ENABLE_AKN */
 
 
 #define OTE_RANDOM_ORACLE 1
