7951e53355c19a05c78100cbd6025a3273702329
Peter Rindal <peterrindal@gmail.com>
2022-11-20 14:24:03 -0800

    header

---
+++ libOTe/Tools/Tungsten/TungstenData.cpp
@@ -0,0 +1,11 @@
+
+#include "TungstenData.h"
+
+namespace osuCrypto
+{
+
+    constexpr std::array<std::array<u16, 4>, 1024> TableTungsten1024x4::data;
+    constexpr std::array<std::array<u16, 7>, 1024> TableTungsten1024x7::data;
+
+    constexpr std::array<std::array<u8, 4>, 128> TableTungsten128x4::data;
+}
