c8a871ea92dcb3708d9816ff8860467831c58d75
Peter Rindal <peterrindal@gmail.com>
2023-08-14 08:02:56 -0700

    Update ExConv accumulate explicit inst

--- libOTe/Tools/ExConvCode/ExConvCodeInstantiations.cpp
+++ libOTe/Tools/ExConvCode/ExConvCodeInstantiations.cpp
@@ -11,4 +11,7 @@ namespace osuCrypto
     template void ExConvCode::dualEncode<u8>(span<u8> e, span<u8> w);
     template void ExConvCode::dualEncode2<block, u8>(span<block>, span<u8> e);
     template void ExConvCode::dualEncode2<block, block>(span<block>, span<block> e);
+
+    template void ExConvCode::accumulate<block, u8>(span<block>, span<u8> e);
+    template void ExConvCode::accumulate<block, block>(span<block>, span<u8> e);
 }
