240422c6b0321447d21294a816162ec172643ace
Stan Peceny <skp2140@columbia.edu>
2024-07-01 10:18:38 -0700

    refactoring the code so that minimum distance can be invoked with any expander and multiplier

--- libOTe/Tools/Tungsten/MinimumDistance.h
+++ libOTe/Tools/Tungsten/MinimumDistance.h
@@ -10,19 +10,35 @@
 namespace osuCrypto {
 
     template<typename I, typename R>
-    u64 minimum_distance(std::vector<std::vector<R>> &block_enumerators, u64 n, u64 num_iters) {
+    u64 minimum_distance(u64 expander, u64 multiplier, u64 num_iters,
+                         u64 n, u64 k, u64 sigma) {
+        std::cout << "Computing minimum distance..." << std::endl;
+
+        assert(sigma <= n);
+        assert(num_iters >= 1);
+        assert(k > 1); // need to be expanding
+        assert(n % sigma == 0);
+
+        // Compute all n^2 possible block enumerators
+        // n^2 space... but it is reused at each iteration
+        std::vector<std::vector<Rat>> block_enumerators(n, std::vector<Rat>(n));
+        for (size_t w = 0; w < n; w++) {
+            for (size_t h = 0; h < n; h++) {
+                block_enumerators[w][h] = block_enum<Int, Rat>(w, h, n, sigma);
+            }
+        }
 
         // TODO consider impact of repeater
 
         // Save all n choose w as used at each iteration
         // n space
-        std::vector<I> n_choose_w (n);
+        std::vector<I> n_choose_w(n);
         for (size_t w = 0; w < n; w++) {
             n_choose_w[w] = choose_<I>(n, w);
         }
 
         // 2n space
-        std::vector<std::vector<R>> temp (2, std::vector<R>(n));
+        std::vector<std::vector<R>> temp(2, std::vector<R>(n));
 
         // First iteration is different
         for (size_t h = 0; h < n; h++) {
@@ -55,39 +71,31 @@ namespace osuCrypto {
         return n;
     }
 
-    inline void minimumDistanceMain(oc::CLP& cmd) {
+    inline void minimumDistanceMain(oc::CLP &cmd) {
         u64 n = cmd.getOr("n", 10); // msg/codeword length
         u64 sigma = cmd.getOr("sigma", 2); // window size
+        // expander (0: repeater, 1: block expander)
+        u64 expander = cmd.getOr("expander", 0);
+        // multiplier (0: block, 1: non recursive convolution)
+        // i.e. the enumerator we compute at each iteration
+        u64 multiplier = cmd.getOr("multiplier", 0);
+        u64 k = cmd.getOr("k", 2); // expansion param - how much to expand, e.g. 2x
         u64 num_iters = cmd.getOr("iters", 1); // number of permute & [multiply] iterations
-        // TODO add repeater as the parameter
 
         std::cout << "n: " << n << std::endl;
         std::cout << "sigma: " << sigma << std::endl;
+        std::cout << "expander: " << expander << ", if 0 then repeater, "
+                                                 "if 1 then block"
+                  << std::endl;
+        std::cout << "k (expansion parameter): " << k << std::endl;
+        std::cout << "multiplier: " << multiplier << ", if 0 then block enumerator, "
+                                                     "if 1 then non-recursive convolution enumerator"
+                  << std::endl;
 
-        assert(sigma <= n);
-        assert(num_iters >= 1);
-
-        if (cmd.isSet("block")) {
-            std::cout << "Computing minimum distance for the block construction..." << std::endl;
-
-            assert(n % sigma == 0);
-
-            // Compute all n^2 possible block enumerators
-            // n^2 space... but it is reused at each iteration
-            std::vector<std::vector<Rat>> block_enumerators(n, std::vector<Rat>(n));
-            for (size_t w = 0; w < n; w++) {
-                for (size_t h = 0; h < n; h++) {
-                    block_enumerators[w][h] = block_enum<Int, Rat>(w, h, n, sigma);
-                }
-            }
-
-            u64 block_minimum_distance = minimum_distance<Int, Rat>(block_enumerators, n, num_iters);
-            std::cout << "Block Minimum Distance: " << block_minimum_distance << std::endl;
-        } else if (cmd.isSet("nonrecConv")) {
-            // TODO
-        }
+        u64 min_distance = minimum_distance<Int, Rat>(expander, multiplier,
+                                                          num_iters, n, k, sigma);
+        std::cout << "Minimum Distance: " << min_distance << std::endl;
     }
 }
-
 
 #endif //LIBOTE_MINIMUMDISTANCE_H
