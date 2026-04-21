bfc9ba57305f53dbdbaec521473974ebac2ce999
Peter Rindal <peterrindal@gmail.com>
2026-04-21 14:28:02 -0700

    Update cryptoTools for GCC 15 coroutine fixes

--- build.py
+++ build.py
@@ -1,10 +1,11 @@
 import os
+import subprocess
 import sys
 
 
 folder = os.path.dirname(os.path.realpath(__file__))
 if os.path.exists(folder+"/cryptoTools/CMakeLists.txt") == False:
-    os.system("git submodule update --init")
+    subprocess.check_call("git submodule update --init", shell=True)
 
 import cryptoTools.build
 
@@ -18,4 +19,4 @@ if __name__ == "__main__":
     argv = sys.argv
     replace(argv, "--bitpolymul", "-DENABLE_SILENTOT=ON -DENABLE_BITPOLYMUL=ON")
     replace(argv, "--all", "-DENABLE_ALL_OT=ON")
-    cryptoTools.build.main("libOTe", argv[1:])
+    sys.exit(cryptoTools.build.main("libOTe", argv[1:]))
