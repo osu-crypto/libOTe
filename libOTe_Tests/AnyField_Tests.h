f41f8f32df5d48a72fabdd962d9a3995e7cbb49d
Peter Rindal <peterrindal@gmail.com>
2026-08-27 19:52:01 -0700

    Add ordinary-DPF any-field OLE construction

---
+++ libOTe_Tests/AnyField_Tests.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "cryptoTools/Common/CLP.h"
+
+namespace osuCrypto
+{
+	void AnyField_F9_Test(const CLP& cmd);
+	void AnyField_F9Transform_Test(const CLP& cmd);
+	void AnyField_F3Ole_Test(const CLP& cmd);
+}
--- libOTe_Tests/UnitTests.cpp
+++ libOTe_Tests/UnitTests.cpp
@@ -34,6 +34,7 @@
 #include "libOTe_Tests/Permutation_Tests.h"
 #include "libOTe_Tests/BlkAccCode_Tests.h"
 #include "libOTe_Tests/LogVole_Tests.h"
+#include "libOTe_Tests/AnyField_Tests.h"
 
 
 using namespace osuCrypto;
@@ -43,6 +44,9 @@ namespace tests_libOTe
 
 	TestCollection Tests([](TestCollection& tc)
 		{
+			tc.add("AnyField_F9_Test                       ", AnyField_F9_Test);
+			tc.add("AnyField_F9Transform_Test              ", AnyField_F9Transform_Test);
+			tc.add("AnyField_F3Ole_Test                     ", AnyField_F3Ole_Test);
 
 			tc.add("Cmp_greaterThan_test                    ", Cmp_greaterThan_test);
 			tc.add("Tools_Transpose_Test                    ", Tools_Transpose_Test);
